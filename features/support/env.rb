ba4296f8dff2829c016e402577f09cc17819c443
Eric Kessler <morrow748@gmail.com>
2016-02-10 00:20:02 -0500

    Better error feedback during testing.
    Added some exception handling in the Cucumber hooks so that pre-test
    exceptions wouldn't get swallowed up and hidden by Cucumber.

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,18 +10,30 @@ require File.dirname(__FILE__) + '/../../lib/cuke_modeler'
 
 
 Before do
-  @default_file_directory = "#{File.dirname(__FILE__)}/../temp_files"
-  @default_feature_file_name = 'test_feature.feature'
-  @default_step_file_name = 'test_steps.rb'
-  @test_file_directory = "#{File.dirname(__FILE__)}/../test_files"
-  @test_step_file_location = "#{@default_file_directory}/#{@default_step_file_name}"
-  @spec_directory = "#{File.dirname(__FILE__)}/../../spec"
+  begin
+    @default_file_directory = "#{File.dirname(__FILE__)}/../temp_files"
+    @default_feature_file_name = 'test_feature.feature'
+    @default_step_file_name = 'test_steps.rb'
+    @test_file_directory = "#{File.dirname(__FILE__)}/../test_files"
+    @test_step_file_location = "#{@default_file_directory}/#{@default_step_file_name}"
+    @spec_directory = "#{File.dirname(__FILE__)}/../../spec"
 
-  FileUtils.mkdir(@default_file_directory)
+    FileUtils.mkdir(@default_file_directory)
+  rescue => e
+    $stdout.puts 'Problem caught in Before hook!'
+    $stdout.puts "Type: #{e.class}"
+    $stdout.puts "Message: #{e.message}"
+  end
 end
 
 After do
-  FileUtils.remove_dir(@default_file_directory, true)
+  begin
+    FileUtils.remove_dir(@default_file_directory, true)
+  rescue => e
+    $stdout.puts 'Problem caught in After hook!'
+    $stdout.puts "Type: #{e.class}"
+    $stdout.puts "Message: #{e.message}"
+  end
 end
 
 
