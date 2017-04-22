2c50f4c733b30f930bfb1207b39739ef76957b33
Eric Kessler <morrow748@gmail.com>
2017-04-22 10:24:52 -0400

    JRuby fix.
    Attempting to avoid stack level problems by turning of a 'feature' of
    newer JVMs.

--- lib/cuke_modeler/models/directory.rb
+++ lib/cuke_modeler/models/directory.rb
@@ -53,8 +53,6 @@ module CukeModeler
 
 
     def process_directory(directory_path)
-puts "processing directory path: #{directory_path}"
-
       directory_data = {'path' => directory_path,
                         'directories' => [],
                         'feature_files' => []
