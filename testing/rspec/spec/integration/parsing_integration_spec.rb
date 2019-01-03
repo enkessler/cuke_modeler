9b36956a3fb0841b62317e375bf7f1feba94d455
Eric Kessler <morrow748@gmail.com>
2019-01-02 22:47:01 -0500

    Fix CI
    Fixed some code that isn't valid with older versions of Ruby.

--- testing/rspec/spec/integration/parsing_integration_spec.rb
+++ testing/rspec/spec/integration/parsing_integration_spec.rb
@@ -15,7 +15,7 @@ describe 'Parsing, Integration' do
       begin
         Gem.loaded_specs['gherkin'].instance_variable_set(:@version, unknown_version)
 
-        expect { load "#{__dir__}/../../../../lib/cuke_modeler/parsing.rb" }.to raise_error("Unknown Gherkin version: '0.0.0'")
+        expect { load "#{File.dirname(__FILE__)}/../../../../lib/cuke_modeler/parsing.rb" }.to raise_error("Unknown Gherkin version: '0.0.0'")
       ensure
         Gem.loaded_specs['gherkin'].instance_variable_set(:@version, original_version)
       end
