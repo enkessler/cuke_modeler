ef0386849931afe9dc85319f5f1003ff3db82a03
Alexander Jeurissen <alexander@jeurissen.email>
2020-12-24 01:26:30 +0100

    remove fingerprint_children as it was too specific

--- lib/cuke_modeler/fingerprint.rb
+++ lib/cuke_modeler/fingerprint.rb
@@ -6,13 +6,14 @@ module CukeModeler
   # A mix-in module containing methods used by models that represent an element that has a name.
 
   module Fingerprint
+
     # NOTE: create a fingerprint for the given model using Digest::MD5
     def fingerprint
       # NOTE: yield the result value of the block as the argument to .hexdigest
       if block_given?
         value = yield self
 
-        # NOTE: The block returned nil, nothing to hexdigest here
+        # NOTE: The block returned nil, nothing to digest here
         return nil unless value.present?
 
         return Digest::MD5.hexdigest(value)
@@ -22,26 +23,6 @@ module CukeModeler
       return Digest::MD5.hexdigest(to_s)
     end
 
-    # NOTE: create a fingerprint for the given model's children using Digest::MD5
-    def fingerprint_children(depth = 0)
-      if children.empty?
-        fail 'dont invoke fingerprint_children on a model without children' if depth == 0
-
-        return fingerprint { |m| yield m } if block_given?
-        return fingerprint
-      end
-
-      # NOTE: aggregate the fingerprints of all it's children
-      fingerprints = children.map do |child|
-        if block_given?
-          child.fingerprint_children(depth+1) { |c| yield c }
-        else
-          child.fingerprint_children(depth+1)
-        end
-      end.compact
-
-      # NOTE: create a .hexdigest of the combined fingerprint of all children
-      Digest::MD5.hexdigest(fingerprints.join)
-    end
   end
+
 end
--- testing/rspec/spec/integration/fingerprint_integration_spec.rb
+++ testing/rspec/spec/integration/fingerprint_integration_spec.rb
@@ -3,208 +3,47 @@ require "#{File.dirname(__FILE__)}/../spec_helper"
 describe 'Fingerprint, Integration' do
 
   describe '.fingerprint' do
-    describe 'An object without children' do
 
-      let(:model) { CukeModeler::Step.new }
-      before do
-        model.keyword = 'Given'
-        model.text = 'I am testing the fingerprint'
-      end
-
-      describe 'getting a fingerprint without args' do
-
-        it 'returns the Digest::MD5 of the to_s representation' do
-          expect(model.fingerprint).to eq(Digest::MD5.hexdigest(model.to_s))
-        end
-
-      end
-
-
-      describe 'getting a fingerprint with a block' do
-
-        it 'returns the Digest::MD5 of the block return value' do
-          fingerprint = model.fingerprint do |m|
-            m.text
-          end
-
-          expect(fingerprint).to eq(Digest::MD5.hexdigest(model.text))
-        end
-
-      end
-    end
-
-    describe 'An object with children' do
-
-      let(:model) { CukeModeler::Scenario.new }
-
-      before do
-        model.name = 'Testing the fingerprint'
-
-        model.steps = 3.times.map do |i|
-          step = CukeModeler::Step.new
-          step.keyword = 'When'
-          step.text = "Step #{i}"
-          step
-        end
-
-        model.tags = 2.times.map do |i|
-          tag = CukeModeler::Tag.new
-          tag.name = "Tag #{i}"
-          tag
-        end
-      end
-
-      describe 'getting a fingerprint without args' do
-
-        it 'returns the Digest::MD5 of the to_s representation' do
-          expect(model.fingerprint).to eq(Digest::MD5.hexdigest(model.to_s))
-        end
-
-      end
-
-
-      describe 'getting a fingerprint with a block' do
-
-        it 'returns the Digest::MD5 of the block return value' do
-          fingerprint = model.fingerprint do |m|
-            m.name
-          end
-
-          expect(fingerprint).to eq(Digest::MD5.hexdigest(model.name))
-        end
-
-      end
-
-    end
-
-  end
-
-  describe '.fingerprint_children' do
-
-    describe 'An object without children' do
-
-      let(:model) { CukeModeler::Step.new }
-      before do
-        model.keyword = 'Given'
-        model.text = 'I am testing the fingerprint'
-      end
-
-      it 'throws an error' do
-        expect { model.fingerprint_children }.to raise_error(
-          'dont invoke fingerprint_children on a model without children'
-        )
-      end
-
-    end
-
-    describe 'An object with children' do
-
-      let(:model) { CukeModeler::Scenario.new }
-
-      before do
-        model.name = 'Testing the fingerprint'
-
-        model.steps = 3.times.map do |i|
-          step = CukeModeler::Step.new
-          step.keyword = 'When'
-          step.text = "Step #{i}"
-          step
-        end
-
-        model.tags = 2.times.map do |i|
-          tag = CukeModeler::Tag.new
-          tag.name = "Tag #{i}"
-          tag
-        end
-      end
-
-      describe 'getting a fingerprint without args' do
-
-        it 'returns the Digest::MD5 of the to_s representation of its children' do
-          children_fingerprints = model.children.map(&:fingerprint)
-
-          expect(children_fingerprints.compact.count)
-            .to eq(children_fingerprints.count)
-
-          expect(model.fingerprint_children).to eq(Digest::MD5.hexdigest(children_fingerprints.join))
-        end
-
-      end
-
-
-      describe 'getting a fingerprint with a block' do
-
-        it 'returns the Digest::MD5 of the block return value' do
-          block = lambda do |m|
-            if m.respond_to?(:name)
-              m.name
-            elsif m.respond_to?(:text)
-              m.text
-            else
-              m.to_s
-            end
-          end
-
-          children_values = [*model.steps.map(&:text), *model.tags.map(&:name)]
-          children_fingerprints = children_values.map { |v| Digest::MD5.hexdigest(v) }
-
-          fingerprint = model.fingerprint_children(&block)
-
-          expect(fingerprint).to eq(Digest::MD5.hexdigest(children_fingerprints.join))
-        end
-
-      end
-
-      describe 'getting a fingerprint with a block that excludes some children' do
-
-        it 'returns the Digest::MD5 of the block return value' do
-          block = lambda do |m|
-            next unless m.is_a? CukeModeler::Step
-
-            m.text
-          end
-
-          step_values = model.steps.map(&:text)
-          step_fingerprints = step_values.map { |v| Digest::MD5.hexdigest(v) }
-
-          fingerprint = model.fingerprint_children(&block)
-
-          expect(fingerprint).to eq(Digest::MD5.hexdigest(step_fingerprints.join))
-        end
-
-      end
-
-    end
-
-  end
-
-  describe '.fingerprint differs from fingerprint_children' do
-
-    let(:model) { CukeModeler::Scenario.new }
+    let(:model) { CukeModeler::Step.new }
 
     before do
-      model.name = 'Testing the fingerprint'
-
-      model.steps = 3.times.map do |i|
-        step = CukeModeler::Step.new
-        step.keyword = 'When'
-        step.text = "Step #{i}"
-        step
-      end
-
-      model.tags = 2.times.map do |i|
-        tag = CukeModeler::Tag.new
-        tag.name = "Tag #{i}"
-        tag
-      end
+      model.keyword = 'Given'
+      model.text = 'I am testing the fingerprint'
     end
 
-    it do
-      expect(model.fingerprint).not_to eq(model.fingerprint_children)
+    describe 'getting a fingerprint without args' do
+
+      it 'returns the Digest::MD5 of the to_s representation' do
+        expect(model.fingerprint).to eq(Digest::MD5.hexdigest(model.to_s))
+      end
+
+    end
+
+    describe 'getting a fingerprint with a block' do
+
+      it 'returns the Digest::MD5 of the block return value' do
+        fingerprint = model.fingerprint do |m|
+          m.text
+        end
+
+        expect(fingerprint).to eq(Digest::MD5.hexdigest(model.text))
+      end
+
+    end
+
+    describe 'getting a fingerprint with a block that returns nil' do
+
+      it 'returns nil' do
+        fingerprint = model.fingerprint do |m|
+          nil
+        end
+
+        expect(fingerprint).to eq(nil)
+      end
+
     end
 
   end
-
 
 end
 
