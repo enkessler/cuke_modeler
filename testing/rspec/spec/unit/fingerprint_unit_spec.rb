b2fdbb6068221fb74ef7b197005296782aecabc4
Alexander Jeurissen <alexander@jeurissen.email>
2020-12-24 02:15:40 +0100

    Fix specs

--- testing/rspec/spec/unit/fingerprint_unit_spec.rb
+++ testing/rspec/spec/unit/fingerprint_unit_spec.rb
@@ -3,7 +3,7 @@ require "#{File.dirname(__FILE__)}/../spec_helper"
 describe 'Fingerprint, Unit', unit_test: true do
 
   let(:nodule) { CukeModeler::Fingerprint }
-  let(:fingerprint_model) { Object.new.extend(nodule) }
+  let(:model) { Object.new.extend(nodule) }
 
 
   it 'has a fingerprint' do
