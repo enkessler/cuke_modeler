fa1bd49801518e2734de7f382ebcc426a62d5436
Eric Kessler <morrow748@gmail.com>
2023-10-08 19:59:24 -0400

    Improve inline documentation
    Added more documentation to the Background class.

--- lib/cuke_modeler/models/background.rb
+++ lib/cuke_modeler/models/background.rb
@@ -23,26 +23,49 @@ module CukeModeler
     #   Background.new("Background:\n  * a step")
     #
     # @param source_text [String] The Gherkin text that will be used to populate the model
+    # @raise [ArgumentError] If *source_text* is not a String
+    # @return [Background] A new Background instance
     def initialize(source_text = nil)
       @steps = []
 
       super(source_text)
     end
 
-    # Returns *true* if the two models have equivalent steps and *false* otherwise.
+    # TODO: Have (all) models be equivalent if they have the same #to_s output. Would
+    # likely require major version change.
+
+    # Compares this model with another object. Returns *true* if the two objects
+    # have equivalent steps and *false* otherwise.
+    #
+    # @example
+    #   background_1 == background_2
+    #
+    # @param other [Object] The object to compare this model with
+    # @return [Boolean] Whether the two objects are equivalent
     def ==(other)
       return false unless other.respond_to?(:steps)
 
       steps == other.steps
     end
 
-    # Returns the model objects that belong to this model.
+    # Returns the model objects that are children of this model. For a
+    # Background model, these would be any associated Step models.
+    #
+    # @example
+    #   background.children
+    #
+    # @return [Array<Model>] A collection of child models
     def children
       steps
     end
 
-    # Returns a string representation of this model. For a background model,
+    # Returns a string representation of this model. For a Background model,
     # this will be Gherkin text that is equivalent to the background being modeled.
+    #
+    # @example
+    #   background.to_s
+    #
+    # @return [String] A string representation of this model
     def to_s
       text = ''
 
@@ -57,7 +80,16 @@ module CukeModeler
     # See `Object#inspect`. Returns some basic information about the
     # object, including its class, object ID, and its most meaningful
     # attribute. For a background model, this will be the name of the
-    # background.
+    # background. If *verbose* is true, provides default Ruby inspection
+    # behavior instead.
+    #
+    # @example
+    #   background.inspect
+    #   background.inspect(verbose: true)
+    #
+    # @param verbose [Boolean] Whether or not to return the full details of
+    #   the object. Defaults to false.
+    # @return [String] A string representation of this model
     def inspect(verbose: false)
       return super(verbose: verbose) if verbose
 
