47c768ee98db3fdc54a74d66e4fa32510e8fb7d0
Eric Kessler <morrow748@gmail.com>
2023-10-14 03:52:14 -0400

    Improve inline documentation
    Added more documentation to the Rule class.

--- lib/cuke_modeler/models/rule.rb
+++ lib/cuke_modeler/models/rule.rb
@@ -23,6 +23,14 @@ module CukeModeler
 
     # Creates a new Rule object and, if *source_text* is provided, populates the
     # object.
+    #
+    # @example
+    #   Rule.new
+    #   Rule.new("Rule:\nThis is a rule")
+    #
+    # @param source_text [String] The Gherkin text that will be used to populate the model
+    # @raise [ArgumentError] If *source_text* is not a String
+    # @return [Rule] A new Rule instance
     def initialize(source_text = nil)
       @tags = []
       @tests = []
@@ -31,6 +39,11 @@ module CukeModeler
     end
 
     # Returns *true* if the rule contains a background, *false* otherwise.
+    #
+    # @example
+    #   rule.background?
+    #
+    # @return [Boolean] Whether the rule contains a background
     def background?
       !@background.nil?
     end
@@ -38,16 +51,33 @@ module CukeModeler
     alias has_background? background?
 
     # Returns the scenario models contained in the rule.
+    #
+    # @example
+    #   rule.scenarios
+    #
+    # @return [Array<Scenario>] Child Scenario models
     def scenarios
       @tests.select { |test| test.is_a? Scenario }
     end
 
     # Returns the outline models contained in the rule.
+    #
+    # @example
+    #   rule.outlines
+    #
+    # @return [Array<Outline>] Child Outline models
     def outlines
       @tests.select { |test| test.is_a? Outline }
     end
 
-    # Returns the model objects that belong to this model.
+    # Returns the model objects that are children of this model. For a
+    # Rule model, these would be any associated Background, Scenario,
+    # Outline, or Tag models.
+    #
+    # @example
+    #   rule.children
+    #
+    # @return [Array<Background, Scenario, Outline, Tag>] A collection of child models
     def children
       models = tests + tags
       models << background if background
@@ -57,6 +87,11 @@ module CukeModeler
 
     # Returns a string representation of this model. For a rule model,
     # this will be Gherkin text that is equivalent to the rule being modeled.
+    #
+    # @example
+    #   rule.to_s
+    #
+    # @return [String] A string representation of this model
     def to_s
       text = ''
 
@@ -72,6 +107,16 @@ module CukeModeler
     # See `Object#inspect`. Returns some basic information about the
     # object, including its class, object ID, and its most meaningful
     # attribute. For a rule model, this will be the name of the rule.
+    # If *verbose* is true, provides default Ruby inspection behavior
+    # instead.
+    #
+    # @example
+    #   rule.inspect
+    #   rule.inspect(verbose: true)
+    #
+    # @param verbose [Boolean] Whether or not to return the full details of
+    #   the object. Defaults to false.
+    # @return [String] A string representation of this model
     def inspect(verbose: false)
       return super(verbose: verbose) if verbose
 
